a29d086d9d62e6d321530b6af5a7f522356524d9
ahgarciar <75009489+ahgarciar@users.noreply.github.com>
2022-03-08 07:46:15 -0600

    Clase8Feb

---
+++ Assets/Scripts/LookAT_Ex.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAT_Ex : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject objMirar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(objMirar.transform);
+
+        
+        Vector3 pos = objMirar.transform.position;
+        pos.y = 0;
+        this.transform.LookAt(pos);
+        
+
+    }
+}
---
+++ Assets/Scripts/RotacionMouse.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionMouse : MonoBehaviour
+{
+    [SerializeField]
+    float speedX  = 20;
+
+    [SerializeField]
+    float speedY = 10;
+
+    float ejeRotacionObj;
+    float ejeRotacionCam;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    GameObject camara; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        
+    }
+
+    private void FixedUpdate()
+    {
+        ejeRotacionObj = Input.GetAxis("Mouse X");
+        ejeRotacionCam += Input.GetAxis("Mouse Y")*speedX;
+
+        //                x       y                  z
+        transform.Rotate(0, ejeRotacionObj * speedY, 0);
+
+
+        ejeRotacionCam = Mathf.Clamp(ejeRotacionCam, -25, 25);
+        //                                                x              y   z
+        camara.transform.localEulerAngles = new Vector3(-ejeRotacionCam, 0f, 0f);
+
+        
+
+
+
+    }
+}
