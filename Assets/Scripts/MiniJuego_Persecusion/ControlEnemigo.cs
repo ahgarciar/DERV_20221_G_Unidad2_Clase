a013520de5e669c9460c702ca941142f4cbde1cb
ahgarciar <75009489+ahgarciar@users.noreply.github.com>
2022-03-23 07:46:22 -0600

    Clase23Mar

--- Assets/Scripts/MiniJuego_Persecusion/ControlEnemigo.cs
+++ Assets/Scripts/MiniJuego_Persecusion/ControlEnemigo.cs
@@ -7,15 +7,34 @@ public class ControlEnemigo : MonoBehaviour
     [SerializeField]
     public GameObject personajeJugador;
 
+    Vector3 posInicio;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        posInicio = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (personajeJugador != null)
+        { //sigue al personaje
+            transform.LookAt(personajeJugador.transform);
+
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                personajeJugador.transform.position,
+                6f * Time.deltaTime);
+        }
+        else {  //regresa a posicion de inicio
+            transform.LookAt(posInicio);
+
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                posInicio,
+                6f * Time.deltaTime);
+        }
+
     }
 }
